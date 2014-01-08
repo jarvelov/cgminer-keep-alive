f51d86e92cd69c985cc41e6e387e3e5697bf7aab
dbsnurr <dbsnurr@gmail.com>
2014-01-09 00:52:39 +0100

    Update startmine.bat

--- startmine.bat
+++ startmine.bat
@@ -1,4 +1,4 @@
 set logoutput=%1
 cd %~dp0
 setx GPU_MAX_ALLOC_PERCENT 100
-cgminer.exe --scrypt --no-submit-stale -o stratum+tcp://pool:3333 -u myusername.1 -p x -I 19 -g 1 -w 256 --thread-concurrency 15232 2>%logoutput%
+cgminer.exe --scrypt --no-submit-stale -o stratum+tcp://pool:3333 -u myusername.1 -p x 2>%logoutput%

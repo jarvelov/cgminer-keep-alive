3e5eaa2185ca53db0ff8fa8e0fe8e14c62a98b89
dbsnurr <dbsnurr@gmail.com>
2014-01-09 18:10:08 +0100

    Simplified default settings

--- startmine.bat
+++ startmine.bat
@@ -1,4 +1,4 @@
 set logoutput=%1
 cd %~dp0
 setx GPU_MAX_ALLOC_PERCENT 100
-cgminer.exe --scrypt --no-submit-stale -o stratum+tcp://pool:3333 -u myusername.1 -p x 2>%logoutput%
+cgminer.exe -o stratum+tcp://pool:3333 -u myusername.1 -p x 2>%logoutput%
